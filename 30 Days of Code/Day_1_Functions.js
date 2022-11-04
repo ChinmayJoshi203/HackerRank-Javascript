baac24bedddab4006a4569e6220f305ec885188e
Chinmay Joshi <chinmay.joshi@excellarate.com>
2022-11-04 14:26:03 +0530

    Day 1 Functions

---
+++ 30 Days of Code/Day_1_Functions.js
@@ -0,0 +1,34 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+/*
+ * Create the function factorial here
+ */
+function factorial(n)
+{
+    let ans=1
+    if(n<=1 || n>=10) return;
+    for(var i=2;i<=n;i++)
+    ans*=i
+    return ans
+}
